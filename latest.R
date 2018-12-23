a2cf4eae5ba81424d84cd49ae1d762f714f4a527
Garrick Aden-Buie <gadenbuie@mail.usf.edu>
2018-12-23 14:59:29 -0500

    Update URL for Ubuntu dailies

--- latest.R
+++ latest.R
@@ -1,6 +1,6 @@
 #!/usr/bin/Rscript
 
-pg <- httr::content(httr::GET("https://dailies.rstudio.com/rstudioserver/oss/ubuntu/amd64/"), as = "parsed")
+pg <- httr::content(httr::GET("https://dailies.rstudio.com/rstudioserver/oss/ubuntu/x86_64/"), as = "parsed")
 deb <- xml2::xml_attr(xml2::xml_find_all(pg, "//tr//td//a"), "href")[1]
 download.file(deb, method="wget", dest="rstudio-server-daily-amd64.deb")
 
